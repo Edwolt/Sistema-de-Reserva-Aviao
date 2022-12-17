f4680e06a5a90ef564352f2369d4fca826743c69
Italo <46253997+Italolmlp@users.noreply.github.com>
2022-12-17 15:14:34 -0300

    first commit

---
+++ server.py
@@ -0,0 +1,31 @@
+import socket
+
+
+def server_program():
+    # get the hostname
+    host = socket.gethostname()
+    port = 5500  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # accept new connection
+    conn2, address2 = server_socket.accept()
+    print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        data2 = conn2.recv(1024).decode()
+        print("from connected user: " + str(data))
+        print("from connected user 2: " + str(data2))
+        data = input(' -> ')
+        conn.send(data.encode())  # send data to the client
+        conn2.send(data.encode())
+    conn.close()  # close the connection
+    conn2.close()
+
+if __name__ == '__main__':
+    server_program()
