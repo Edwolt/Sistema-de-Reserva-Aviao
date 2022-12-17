ae001b9183fcfdf5f541e36944b53e2f88750bbb
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2022-12-17 17:22:00 -0300

    Interface client

--- client.py
+++ client.py
@@ -1,4 +1,18 @@
 import socket
+import PySimpleGUI as sg
+
+
+class Janela:
+    def __init__(self, name):
+        self.layout = [
+            [sg.Text("Assentos")],
+            [sg.Text("O") for i in range(30)],
+            [sg.Input()],
+        ]
+        self.window = sg.Window(f"Cliente {name}", self.layout)
+
+    def read(self):
+        return self.window.read()
 
 
 def client_program():
@@ -8,18 +22,28 @@ def client_program():
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
-    message = input(" -> ")  # take input
+    janela = Janela(host)
 
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
+    while True:
+        event, values = janela.read()
+        if event == sg.WIN_CLOSED or values == "Exit":
+            client_socket.send("2".encode())
+            break
 
-        print('Received from server: ' + data)  # show in terminal
+        client_socket.send("0".encode())
+        data = client_socket.recv(1024).decode
+        print(data)
 
-        message = input(" -> ")  # again take input
+    # while message.lower().strip() != "bye":
+    #     client_socket.send(message.encode())  # send message
+    #     data = client_socket.recv(1024).decode()  # receive response
+
+    #     print("Received from server: " + data)  # show in terminal
+
+    #     message = input(" -> ")  # again take input
 
     client_socket.close()  # close the connection
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     client_program()
--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 import socket
 import threading
 
+
 def server_thread(conn, address, name):
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
         data = conn.recv(1024).decode()
         print("from connected user: " + str(data))
-        data = input(' -> ')
+        data = input(" -> ")
         conn.send(data.encode())  # send data to the client
     conn.close()  # close the connection
-
 
 
 def server_program():
@@ -21,17 +21,23 @@ def server_program():
     # look closely. The bind() function takes tuple as argument
     server_socket.bind((host, port))  # bind host address and port together
     try:
-       # configure how many client the server can listen simultaneously
-       server_socket.listen(10)
-       id = 1
-       while True:
-           conn, address = server_socket.accept()  # accept new connection
-           print("Connection from: " + str(address))
-           new_thread = threading.Thread(target = server_thread, args=(conn,address,f"Client {id}"))
-           new_thread.start()
-           id += 1
-    finally:
-        server_socket.shutdown(socket.SHUT_RDWR)
+        # configure how many client the server can listen simultaneously
+        server_socket.listen(10)
+        id = 1
+        while True:
+            conn, address = server_socket.accept()  # accept new connection
+            print("Connection from: " + str(address))
+            new_thread = threading.Thread(
+                target=server_thread, args=(conn, address, f"Client {id}")
+            )
+            new_thread.start()
+            id += 1
+    except KeyboardInterrupt:
+        pass
 
-if __name__ == '__main__':
+    print("exited server...")
+    server_socket.shutdown(socket.SHUT_RDWR)
+
+
+if __name__ == "__main__":
     server_program()
