6afadd158c1b6ad78c82efb5c634e4abc37a3d97
Mateus Israel Silva <38586542+Mateusrael@users.noreply.github.com>
2022-12-19 16:52:52 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,17 +2,17 @@ import socket
 import threading
 from servidor.aviao import Aviao
 
-def bool_to_data(poltronas):
+def bool_to_data(poltronas): # transforma os dados booleanos em "L" para livre e "O" para ocupado
     res = ""
     for poltrona in poltronas:
         res += "L" if poltrona else "O"
 
     return res
 
-def data_to_poltronas(data):
+def data_to_poltronas(data): # transforma os dados nos indices das poltronas
     return [int(x) for x in data.split(' ')]
 
-def list_to_data(retorno):
+def list_to_data(retorno): # transforma a lista booleana em "1"s e "0"s
     res = ""
     for valor in retorno:
         res += "1" if valor else "0"
@@ -20,18 +20,18 @@ def list_to_data(retorno):
     return res
 
 def server_thread(conn, address, identificador, aviao):
-    conn.send(bool_to_data(aviao.livres()).encode())
+    conn.send(bool_to_data(aviao.livres()).encode()) # envia os dados das poltronas livres para o cliente
 
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
         data = conn.recv(1024).decode()
         data = str(data)
         print(f"from {identificador}: {data}")
-        if data[0] == '0':
+        if data[0] == '0': # caso não tenha sido feita uma reserva ou cancelamento
             data = bool_to_data(aviao.livres()).encode()
-        elif data[0] == '1':
+        elif data[0] == '1': # caso tenha sido feita uma reserva
             data = list_to_data(aviao.reserva(data_to_poltronas(data[1:]), identificador)).encode()
-        elif data[0] == '2':
+        elif data[0] == '2': # caso tenha sido feito um cancelamento
             data = list_to_data(aviao.cancela_reserva(data_to_poltronas(data[1:]), identificador)).encode()
         else:
             break
