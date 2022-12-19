c73e71235b9c4b75ad2d5ff4ed902544d9722138
Mateus Israel Silva <38586542+Mateusrael@users.noreply.github.com>
2022-12-19 16:51:58 -0300

    Update client.py

--- client.py
+++ client.py
@@ -6,20 +6,20 @@ colunas = 'ABCDEF'
 numero_assentos = 60
 delay_update = 1.0
 
-def id_para_poltrona(id):
+def id_para_poltrona(id): # retorna linha e coluna da poltrona
     return str(id//6) + colunas[id%6]
 
-def create_janela(name):
+def create_janela(name): # cria uma janela
     layout = [[sg.Text("Assentos")]] + [[sg.Button(colunas[i%6], key = ("assento", i), button_color = ('white','green')) for i in range(j,j+3)] + [sg.Text("{:0>2d}".format(1 + (j//6)))] + [sg.Button(colunas[i%6], key = ("assento", i), button_color = ('white','green')) for i in range(j+3,j+6)] for j in range(0,numero_assentos,6)]
     return sg.Window(f"Cliente {name}", layout, finalize=True)
 
-def thread_update(client_socket, janela):
+def thread_update(client_socket, janela): # atualiza a thread a cada segundo
     janela.write_event_value("update", "timer")
     t = threading.Timer(delay_update, thread_update, args=(client_socket,janela))
     t.daemon = True
     t.start()
 
-def update_assentos(janela, data):
+def update_assentos(janela, data): # atualiza as cores da poltronas
     for i, val in enumerate(data):
         if janela[("assento",i)].ButtonColor != ('white','blue'):
             janela[("assento",i)].update(button_color= ('white','green') if val == 'L' else ('white','red'))
@@ -34,7 +34,8 @@ def client_program():
     janela = create_janela(host)
     data = client_socket.recv(1024).decode()
     update_assentos(janela, data)
-    
+
+    # comeca a atualizar a thread a cada segundo
     t = threading.Timer(delay_update, thread_update, args=(client_socket,janela))
     t.daemon = True
     t.start()
@@ -42,6 +43,7 @@ def client_program():
     while True:
         event, values = janela.read()
 
+        # caso a janela seja fechada
         if event == sg.WIN_CLOSED or values == "Exit":
             client_socket.send("Q".encode())
             break
